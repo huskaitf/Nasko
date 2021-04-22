184df9c3a662a8173e31bf2c65e4e09360a66ed6
Deibed Bene <69766360+huskaitf@users.noreply.github.com>
2021-04-21 22:04:39 -0300

    Add files via upload

---
+++ config/developers.js
@@ -0,0 +1,3 @@
+let devlist = ["742378220690604034", "714880395925258322"]
+
+module.exports = devlist;
---
+++ config/response.js
@@ -0,0 +1,17 @@
+let response = ["Sim.",
+    "Não.",
+    "Talvez.",
+    "Não posso opinar sobre isso...",
+    "Sei de nada...",
+    "Provavelmente sim.",
+    "Provavelmente não.",
+    "Não sei...",
+    "Eu diria que sim.",
+    "Eu diria que não.",
+    "Hmm acho que não.",
+    "Hmm acho que sim.",
+    "Isso é meio obvio.",
+    "Concerteza!"
+];
+
+module.exports = response;
